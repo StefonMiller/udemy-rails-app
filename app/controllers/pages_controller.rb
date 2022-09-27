936efdfd015c51d703ce46141bf1fd83036d8b2e
unknown <stefonplays@gmail.com>
2022-09-26 20:46:59 -0400

    Created home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+    def home
+        # By default, controller renders method_name.html.erb
+        # file in /view/controller_name
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/"). Default controller is 
+  # application_controller, can omit the _controller suffix
+  # when specifying which one to use. # suffix specifies the
+  # action to run in the controller
+  root "pages#home"
+
+  # Request -> routes.rb -> appropriate controller -> appropriate view template
+end
